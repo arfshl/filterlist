ca777b7c1e894b4a7363f30be73153564cdab6f9
Alif Putra Winata <arfshl@protonmail.com>
2026-03-24 05:26:12 +0700

    put handcrafted filter to the exception

--- fop.py
+++ fop.py
@@ -58,7 +58,7 @@ BLANKPATTERN = re.compile(r"^\s*$")
 COMMITPATTERN = re.compile(r"^(A|M|P)\:\s(\((.+)\)\s)?(.*)$")
 
 # List the files that should not be sorted, either because they have a special sorting system or because they are not filter files
-IGNORE = ("a.txt", "b.txt", "svc.txt", "LICENSE", "README.md", "c.template")
+IGNORE = ("a.txt", "b.txt", "svc.txt", "LICENSE", "README.md", "c.template", "personal-ubo.txt", "personal-agh.txt")
 
 # List all Adblock Plus, uBlock Origin and AdGuard options (excepting domain, which is handled separately), as of version 1.3.9
 KNOWNOPTIONS = ("badfilter", "collapse", "csp", "document", "elemhide",
